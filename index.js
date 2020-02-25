81f8d0ef42d521c77ecdb19f2a7c74b3c37b6b2f
Omar Farah <farah.omar8787@gmail.com>
2020-02-25 17:52:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-var kittens = [] //define your array here
+var kittens = ["Milo","Otis","Garfield"]
+function destructivelyAppendKitten(name){
+  return kittens.push("Ralph")
+}
+
+function destructivelyRemoveLastKitten(name){
+  return kittens.pop()
+
+}
+
+function destructivelyRemoveFirstKitten(name){
+  return kittens.shift()
+}
+const newCat = ["Broom"];
+function appendKitten(name){
+//  array3 = kittens.concat(newCat)
+  return array3 = kittens.concat(newCat)
+}
+
 
 // Add your functions and code here
